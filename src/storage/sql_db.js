d70fda83908f5c89923aa93772f98765e1674bf9
Anri Asaturov <anri82@gmail.com>
2016-03-26 01:52:33 +0200

    node-sqlite support

--- src/storage/sql_db.js
+++ src/storage/sql_db.js
@@ -12,31 +12,37 @@ Peerio.SqlDB.init = function () {
     'use strict';
 
     Peerio.SqlDB.init = undefined;
-
-    var plugin = window.sqlitePlugin;
-    // mockup for debugging in browser
-    if (!plugin && window.PeerioDebug) {
+    //-- PUBLIC API ----------------------------------------------------------------------------------------------------
+    Peerio.SqlDB = {
+        openUserDB: openUserDB,
+        deleteUserDB: deleteUserDB,
+        closeAll: closeAllUserDatabases,
+        openSystemDB: openSystemDB
+    };
+    //------------------------------------------------------------------------------------------------------------------
+    var plugin;
+    // in-browser, used for development only, not 100% compatible with native sqlite3, not encrypted
+    function enableWebSqlPlugin() {
         plugin = {
             openDatabase: function (params, success, fail) {
                 var ret = window.openDatabase(params.name, '1', params.name, 5 * 1024 * 1024);
                 var origTran = ret.transaction.bind(ret);
+
                 ret.executeSql = function (statement, values, resolve, reject) {
                     return origTran(function (tx) {
                         return tx.executeSql(statement, values, (tx, res)=>resolve(res), reject);
                     });
                 };
 
-                ret.open = function () {
-                    console.log('DB OPEN MOCK');
-                };
-
                 ret.close = function (resolve) {
                     console.log('DB CLOSE MOCK');
                     resolve();
                 };
+
                 ret.abortAllPendingTransactions = function () {
                     console.log('DB abortAllPendingTransactions MOCK');
                 };
+
                 window.setTimeout(()=>success(ret), 0);
                 return ret;
 
@@ -48,17 +54,80 @@ Peerio.SqlDB.init = function () {
         };
     }
 
-    //-- PUBLIC API ----------------------------------------------------------------------------------------------------
-    Peerio.SqlDB = {
-        openUserDB: openUserDB,
-        deleteUserDB: deleteUserDB,
-        closeAll: closeAllUserDatabases,
-        openSystemDB: openSystemDB
-    };
-    //------------------------------------------------------------------------------------------------------------------
+
+    // mobile sqlite plugin is somewhat mimicking websql api
+    function enableCordovaPlugin() {
+        plugin = window.sqlitePlugin;
+    }
+
+    // node bindings for sqlite
+    function enableNodePlugin() {
+        plugin = nodeRequire('sqlite3');
+
+        plugin.openDatabase = function (params, success, fail) {
+            try {
+                var setupDb = function () {
+                    // sql params do not work with pragma, but it is safe to construct sql string here
+                    // because key is a generated hash and user input is never passed here
+                    db.run("PRAGMA KEY = '" + params.key + "'");
+                    db.run("PRAGMA CIPHER = 'aes-256-cbc'");
+                    db.run("PRAGMA cipher_page_size = 2048");
+
+                    // todo
+                    db.transaction = function (fn, fail, win) {
+                        db.exec('BEGIN');
+                        fn(db);
+                        db.exec('COMMIT');
+                        win();
+                    };
+
+                    db.executeSql = function (sql, params, win, fail) {
+                        db.all(sql, params, function (error, data) {
+                            if (error) {
+                                fail(error);
+                                return;
+                            }
+                            data.item = function (i) {
+                                return data[i];
+                            };
+                            win({rows: data});
+                        })
+                    };
+                    //todo
+                    db.abortAllPendingTransactions = function(){};
+
+                    setTimeout(()=>success(db), 0);
+                };
+
+                var db = new plugin.Database(params.name, plugin.OPEN_READWRITE | plugin.OPEN_CREATE, function (error) {
+                    if (error)
+                        setTimeout(()=>fail(error), 0);
+                    else
+                        setupDb();
+                });
+                return db;
+            } catch (err) {
+                fail(err);
+            }
+        };
+
+        plugin.deleteDatabase = function (name, resolve) {
+            // todo delete db file
+            resolve();
+        }
+    }
+
+    // deciding on plugin
+    if (window.PeerioDebug) {
+        enableWebSqlPlugin();
+    } else if (window.sqlitePlugin) {
+        enableCordovaPlugin();
+    } else {
+        enableNodePlugin();
+    }
 
     promisifyStatic();
-    // TODO: commenting out because closeAll is called from Peerio.initAPI and IS ASYNC 
+    // TODO: commenting out because closeAll is called from Peerio.initAPI and IS ASYNC
     // security measure, to make sure previous database is closed after app reload
     // closeAllUserDatabases();
 
@@ -108,10 +177,10 @@ Peerio.SqlDB.init = function () {
         //    L.info('Using plugin capabilities to close databases');
         //    return plugin.closeAll();
         //} else {
-            // TODO: implement for all platforms
-            L.info('Using manual db closing');
-            var tmpdbName = '___peerio__tmp__system__db___';
-            return plugin.openDatabase(tmpdbName, '')
+        // TODO: implement for all platforms
+        L.info('Using manual db closing');
+        var tmpdbName = '___peerio__tmp__system__db___';
+        return plugin.openDatabase(tmpdbName, '')
             .then(tmpdb => {
                 var dblist = tmpdb.openDBs;
                 setTimeout(()=> {
@@ -126,14 +195,14 @@ Peerio.SqlDB.init = function () {
                 Promise.each(Object.keys(dblist), dbname => {
                     if (dbname === tmpdbName || dbname === systemDbName) return;
                     plugin.openDatabase(dbname)
-                    .then(db => {
-                        db.abortAllPendingTransactions();
-                        return db.close();
-                    });
+                        .then(db => {
+                            db.abortAllPendingTransactions();
+                            return db.close();
+                        });
                 });
             });
-       // }
-   }
+        // }
+    }
 
     //-- PROMISIFICATORS -----------------------------------------------------------------------------------------------
     // we are not afraid of anything now. replace the functions with what suits us the best
@@ -166,12 +235,12 @@ Peerio.SqlDB.init = function () {
 
     function promisifyDb(db) {
         //NOTE: db.open is used internally by sqlite plugin, but function signature is the same, so - no harm
-        var originalOpen = db.open.bind(db);
+        //var originalOpen = db.open.bind(db);
         var originalClose = db.close.bind(db);
         var originalTransaction = db.transaction.bind(db);
         var originalExecute = db.executeSql.bind(db);
 
-        db.open = () => new Promise((resolve, reject) => originalOpen(resolve, reject));
+        //db.open = () => new Promise((resolve, reject) => originalOpen(resolve, reject));
         db.close = () => new Promise((resolve, reject) => originalClose(resolve, reject));
         db.transaction = fn => new Promise((resolve, reject) => {
             originalTransaction(tx => fn(tx), reject, resolve);// mixed order in api
@@ -188,4 +257,5 @@ Peerio.SqlDB.init = function () {
     }
 
 
-};
+}
+;
