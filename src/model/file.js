f6774581640b0f3c798295d6325cebe661fe0426
Anri Asaturov <anri82@gmail.com>
2016-04-07 13:23:34 +0300

    Localized file statuses

--- src/model/file.js
+++ src/model/file.js
@@ -243,9 +243,10 @@ var Peerio = this.Peerio || {};
 
 
     var DL_STATE = {DOWNLOADING: 0, DECRYPTING: 1, SAVING: 2};
-    var DLStateNames = {0: 'Downloading', 1: 'Decrypting', 2: 'Saving'};
+    // locale resource names
+    var DLStateNames = {0: 'fileState_downloading', 1: 'fileState_decrypting', 2: 'fileState_saving'};
     var UL_STATE = {READING: 0, ENCRYPTING: 1, UPLOADING_META: 2, UPLOADING_CHUNKS: 3};
-    var ULStateNames = {0: 'Reading', 1: 'Encrypting', 2: 'Uploading encrypted metadata', 3: 'Uploading chunks'};
+    var ULStateNames = {0: 'fileState_reading', 1: 'fileState_encrypting', 2: 'fileState_uploadingMetadata', 3: 'fileState_uploading'};
     //-- PUBLIC API ------------------------------------------------------------------------------------------------------
     /**
      * Call Peerio.File() to create empty file object
